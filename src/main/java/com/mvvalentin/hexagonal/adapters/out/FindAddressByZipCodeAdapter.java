f33b82b35372c1edbde5f89d2a986ad84c4ad71e
Monicat263 <mda263@bol.com.br>
2026-02-25 16:07:14 -0300

    feat: configurar MapStruct e implementar adaptador de endereço - Atualizado build.gradle com dependências do MapStruct e annotation processors. - Criado AddressResponseMapper para conversão de AddressResponse para Address. - Implementado FindAddressByZipCodeAdapter integrando Feign Client e Mapper.

---
+++ src/main/java/com/mvvalentin/hexagonal/adapters/out/FindAddressByZipCodeAdapter.java
@@ -0,0 +1,25 @@
+package com.mvvalentin.hexagonal.adapters.out;
+
+import com.mvvalentin.hexagonal.adapters.out.client.FindAddressByZipCodeClient;
+import com.mvvalentin.hexagonal.adapters.out.client.mapper.AddressResponseMapper;
+import com.mvvalentin.hexagonal.application.core.domain.Address;
+import com.mvvalentin.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FindAddressByZipCodeAdapter implements FindAddressByZipCodeOutputPort {
+
+    // injetar o Client
+    @Autowired
+    private FindAddressByZipCodeClient findAddressByZipCodeClient;
+
+    @Autowired
+    private AddressResponseMapper addressResponseMapper;
+
+    @Override
+    public Address find(String zipCode) {
+        var addressResponse = findAddressByZipCodeClient.find(zipCode);
+        return addressResponseMapper.toAddress(addressResponse);
+    }
+}
