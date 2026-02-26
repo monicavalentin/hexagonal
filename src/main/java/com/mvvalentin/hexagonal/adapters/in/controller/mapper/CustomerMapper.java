cd41ecd0596b1418cec42ec9aad7b1f18563b7dc
Monicat263 <mda263@bol.com.br>
2026-02-26 12:29:21 -0300

    refatoração

--- src/main/java/com/mvvalentin/hexagonal/adapters/in/controller/mapper/CustomerMapper.java
+++ src/main/java/com/mvvalentin/hexagonal/adapters/in/controller/mapper/CustomerMapper.java
@@ -1,17 +1,18 @@
 package com.mvvalentin.hexagonal.adapters.in.controller.mapper;
 
-import com.mvvalentin.hexagonal.adapters.in.controller.request.CustomerResquest;
+import com.mvvalentin.hexagonal.adapters.in.controller.request.CustomerRequest;
 import com.mvvalentin.hexagonal.application.core.domain.Customer;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 
 public interface CustomerMapper {
-
+    @BeanMapping(ignoreByDefault = true)  // evita erros quando novos campos forem adicionados na entidade.
     @Mapping(target = "id", ignore = true) // utilizado para ignorar campos da classe Customer
     @Mapping(target = "address", ignore = true)
-    @Mapping(target = "isValidCpf", ignore = true)
-    Customer toCostumer(CustomerResquest customerResquest);
+    @Mapping(target = "validCpf", ignore = true)
+    Customer toCustomer(CustomerRequest customerResquest);
 
 }
--- src/main/java/com/mvvalentin/hexagonal/application/core/useCase/InsertCustomerUseCase.java
+++ src/main/java/com/mvvalentin/hexagonal/application/core/useCase/InsertCustomerUseCase.java
@@ -1,25 +1,26 @@
 package com.mvvalentin.hexagonal.application.core.useCase;
 
 import com.mvvalentin.hexagonal.application.core.domain.Customer;
-import com.mvvalentin.hexagonal.application.ports.in.InsertCustomerInputPort;
-import com.mvvalentin.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
+import com.mvvalentin.hexagonal.application.ports.in.InsertCustomerInPutPort;
+import com.mvvalentin.hexagonal.application.ports.out.FindAddressByZipCodeOutPutPort;
 import com.mvvalentin.hexagonal.application.ports.out.InsertCustomerOutPutPort;
+import org.springframework.stereotype.Service;
 
-    //  Use Case responsável pela lógica de negócio de inserção de um novo cliente.Segue o princípio
+//  Use Case responsável pela lógica de negócio de inserção de um novo cliente.Segue o princípio
     //  da inversão de dependência, comunicando-se com o mundo externo  através de interfaces (Output Ports).
-
-public class InsertCustomerUseCase implements InsertCustomerInputPort {
+@Service
+public class InsertCustomerUseCase implements InsertCustomerInPutPort  {
     // Portas de saída: O Use Case não sabe QUEM busca o endereço ou QUEM salva o cliente,
     // ele apenas sabe que existe um contrato para isso.
 
-    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutpitPort;
+    private final FindAddressByZipCodeOutPutPort findAddressByZipCodeOutPutPort;
     private final InsertCustomerOutPutPort insertCustomerOutPutPort;
 
       //Construtor para injeção de dependências.
 
-    public InsertCustomerUseCase(FindAddressByZipCodeOutputPort findAddressByZipCodeOutpitPort ,
+    public InsertCustomerUseCase(FindAddressByZipCodeOutPutPort findAddressByZipCodeOutputPort,
                                  InsertCustomerOutPutPort insertCustomerOutPutPort) {
-        this.findAddressByZipCodeOutpitPort = findAddressByZipCodeOutpitPort;
+        this.findAddressByZipCodeOutPutPort = findAddressByZipCodeOutputPort;
         this.insertCustomerOutPutPort = insertCustomerOutPutPort;
     }
 
@@ -30,7 +31,7 @@ public class InsertCustomerUseCase implements InsertCustomerInputPort {
     @Override
     public void insert(Customer customer, String zipCode){
         // 1. Busca as informações de endereço via integração externa (ex: API de Correios/ViaCEP)
-        var andress = findAddressByZipCodeOutpitPort.find(zipCode);
+        var andress = findAddressByZipCodeOutPutPort.find(zipCode);
         // 2. Associa o endereço retornado ao objeto cliente
         customer.setAddress(andress);
         // 3. Persiste o cliente no banco de dados através do adaptador de saída correspondente
