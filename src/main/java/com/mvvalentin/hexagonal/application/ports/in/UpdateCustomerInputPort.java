7dd65d01f9fb38c2eb3bafebf4362500cef263ef
Monicat263 <mda263@bol.com.br>
2026-03-02 21:21:14 -0300

    implementando do método @PUT

---
+++ src/main/java/com/mvvalentin/hexagonal/application/ports/in/UpdateCustomerInputPort.java
@@ -0,0 +1,7 @@
+package com.mvvalentin.hexagonal.application.ports.in;
+
+import com.mvvalentin.hexagonal.application.core.domain.Customer;
+
+public interface UpdateCustomerInputPort {
+    void update(Customer customer, String zipCode);
+}
