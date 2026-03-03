49d30ab5f049907e1d96dccc9d741da8a418ac75
Monicat263 <mda263@bol.com.br>
2026-03-02 22:20:11 -0300

    refatoração: remoção de espaços

--- src/main/java/com/mvvalentin/hexagonal/adapters/out/repository/CustomerRepository.java
+++ src/main/java/com/mvvalentin/hexagonal/adapters/out/repository/CustomerRepository.java
@@ -4,4 +4,5 @@ import com.mvvalentin.hexagonal.adapters.out.repository.entity.CustomerEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CustomerRepository extends MongoRepository<CustomerEntity, String> {
+
 }
